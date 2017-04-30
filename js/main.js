77c53f4a78289b448d9cfb5532cb9491ff85a14d
ilia24 <iliademertchian24@gmail.com>
2017-04-30 14:40:35 -0400

    Add if else statement to allow light to be turned on and off

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(function() {
 
 $('.plate').on('click', function() {
-  $('.status').text("It's so DARK in here!");
 
+if ($('body').attr('class') === 'light') {
+  $('.status').text("Who turned out the lights?");
+  $('.switch').attr('class', 'switch off');
+  $('body').attr('class', 'dark')
+} else {
+  $('.status').text("It's so bright in here!");
+  $('.switch').attr('class', 'switch on');
+  $('body').attr('class', 'light')
+
+}
 })
 });
